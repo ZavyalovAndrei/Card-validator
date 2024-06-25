dc7ff5aeb5a5416fa2faf2f7f0410c2d8af14c78
ZavyalovAndrei <zaver1983@gmail.com>
2024-06-25 22:47:55 +0200

    modyfy checkCardNumber.test.js

--- src/js/e2e/checkCardNumber.test.js
+++ src/js/e2e/checkCardNumber.test.js
@@ -1,33 +1,47 @@
-import puppeteer from "puppeteer";
+import puppeteer from 'puppeteer';
+
 
 describe('check card number', () => {
     let browser;
     let page;
 
-    beforeEach(async () =>{
+    beforeEach(async () => {
         browser = await puppeteer.launch({
             headless: false,
             slowMo: 100,
             devtools: true,
         });
+
         page = await browser.newPage();
     });
+
     test('Form input should add .valid class if card number is valid', async () => {
 await page.goto('http://localhost:9000');
 
-await page.waitFor('.card-form-widget');
+await page.waitForSelector('.card-form-widget');
 
-/*const form = await page.$('.card-form-widget');
+const form = await page.$('.card-form-widget');
 const input = await form.$('.input');
 const submit = await form.$('.submit');
-
-await input.type('4111111111111111');
+await input.type('4111 1111 1111 1111');
 await submit.click();
-
-await page.waitFor('.message .valid')*/
+await page.waitForSelector('.card-form-widget .input.valid');
     });
-afterAll(async () => {
-    await browser.close();
-})
 
+    test('Form input should add .invalid class if card number is invalid', async () => {
+        await page.goto('http://localhost:9000');
+        
+        await page.waitForSelector('.card-form-widget');
+        
+        const form = await page.$('.card-form-widget');
+        const input = await form.$('.input');
+        const submit = await form.$('.submit');
+        await input.type('3012 1868 6445 5876');
+        await submit.click();
+        await page.waitForSelector('.card-form-widget .input.invalid');
+            });
+
+    afterEach(async () => {
+        await browser.close();
+    });
 });
